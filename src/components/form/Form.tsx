557372867857619888081e14aa114858d78ad276
Alex <cptmutchell@gmail.com>
2023-05-28 23:46:47 +0300

    test

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -3,9 +3,10 @@ import "./Form.scss"
 import {useTelegram} from "@/hooks/useTelegram";
 import {IOrder} from "@/Types";
 import {Button} from "@/components/button/Button";
+import {Box, TextField, Typography} from "@mui/material";
 
 export const Form:React.FC = () => {
-    const {tg,user} = useTelegram()
+    const {tg} = useTelegram()
     const [order, setOrder] = useState<IOrder>(Object)
     const onChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
 
@@ -13,31 +14,22 @@ export const Form:React.FC = () => {
     }
     const handleSubmit = (e:any) =>{
         e.preventDefault()
-        console.log(order)
         tg.sendData(JSON.stringify(order))
     }
     return (
         <>
 
-            {/*<Box component="form" className="tg-form" onSubmit={handleSubmit}>*/}
-            {/*    <Typography variant="h3" sx={{color:"white"}}>Введите данные</Typography>*/}
-            {/*    <TextField onChange={onChange} name="address" required label="Адрес"></TextField>*/}
-            {/*    <TextField onChange={onChange} name="brand" required label="Бренд"></TextField>*/}
-            {/*    <TextField onChange={onChange} name="volume" required label="Обьем"></TextField>*/}
-            {/*    <TextField onChange={onChange} name="tel_num" required label="Телефон"></TextField>*/}
-            {/*    <TextField onChange={onChange} name="promo_code"  label="Промокод"></TextField>*/}
-            {/*    <Button type="submit" >Отправить заявку</Button>*/}
+            <Box component="form" className="tg-form" onSubmit={handleSubmit}>
+                <Typography variant="h3" sx={{color:"white"}}>Введите данные</Typography>
+                <TextField onChange={onChange} name="address" required label="Адрес"></TextField>
+                <TextField onChange={onChange} name="brand" required label="Бренд"></TextField>
+                <TextField onChange={onChange} name="volume" required label="Обьем"></TextField>
+                <TextField onChange={onChange} name="tel_num" required label="Телефон"></TextField>
+                <TextField onChange={onChange} name="promo_code"  label="Промокод"></TextField>
+                <Button type="submit" >Отправить заявку</Button>
 
-            {/*</Box>*/}
+            </Box>
 
-                <div>
-                <input  name="aroma" placeholder="Аромат" value={order.aroma} onChange={onChange}/>
-                <input  name="brand" placeholder="Бренд" value={order.brand} onChange={onChange}/>
-                <input  name="volume" placeholder="Обьем" value={order.volume} onChange={onChange}/>
-                <input  name="tel_num" placeholder="Телефон" value={order.tel_num} onChange={onChange}/>
-                <input  name="promo_code" placeholder="Промокод" value={order.promo_code} onChange={onChange}/>
-                <Button onClick={()=>tg.sendData(JSON.stringify(order))}>Отправить заявку</Button>
-                </div>
 
         </>
     )
